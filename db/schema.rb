25608f5b8b4241a0cbdc6f6a54d99420b87911a6
Genesis <genesisreyes.9400@gmail.com>
2026-01-06 18:59:57 -0600

    feat: agregar modelo Book con migración y schema actualizado

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_06_235950) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "books", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.date "end_date"
+    t.string "last_name_author"
+    t.string "name_author"
+    t.date "start_date"
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
 end
