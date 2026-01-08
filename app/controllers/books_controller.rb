503fbc8babfa8d057273766ce260c61b7510d4bd
Genesis <genesisreyes.9400@gmail.com>
2026-01-08 09:32:05 -0600

    feat: agregar book_controllers con CRUD completo y rutas

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,61 @@
+class BooksController < ApplicationController
+    # Función para listar todos los libros
+    def index
+        @books = Book.all
+    end
+
+    # Función para mostrar un libro específico
+    def show
+        @book = Book.find(params[:id])
+    end
+
+    # Función para inicializar un nuevo libro
+    def new
+        @book = Book.new
+    end
+
+    # Función para crear un libro en la base de datos
+    def create
+        @book = Book.new(book_params)
+        if @book.save
+            redirect_to @book # redirecciona a la vista del libro recién creado
+        else
+            render 'new' # vuelve a mostrar el formulario de nuevo libro
+        end
+    end
+    
+    # Formulario para editar un libro existente
+    def edit
+        @book = Book.find(params[:id])
+    end
+
+    # Función para actualizar un libro existente
+    def update
+        @book = Book.find(params[:id])
+        if @book.update(book_params)
+            redirect_to @book # redirecciona a la vista del libro actualizado
+        else
+            render 'edit' # vuelve a mostrar el formulario de edición
+        end
+    end
+
+    # Función para eliminar un libro
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to books_path
+    end
+
+    # book_params es un método privado para permitir solo los parámetros permitidos
+    private
+    def book_params
+        params.require(:book).permit(
+            :title, 
+            :name_author, 
+            :last_name_author, 
+            :start_date, 
+            :end_date
+        )
+    end
+end
+
