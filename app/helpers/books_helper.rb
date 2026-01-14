42644c19d16094436dbf9bbfed7cb1d22b3f263d
Genesis <genesisreyes.9400@gmail.com>
2026-01-14 12:22:00 -0600

    udapte: depurar helpers - verificar que helpers podrian ser validaciones en models

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,8 +1,4 @@
 module BooksHelper
-    # Concatenar el nombre y apellido del autor
-    def author_full_name(book)
-        "#{book.name_author} #{book.last_name_author}"
-    end
 
     # Formatear la fecha en formato dd/mm/yyyy o mostrar un guion si es nula
     def format_date(date)
@@ -13,12 +9,8 @@ module BooksHelper
     def period_lecture(book)
         "#{format_date(book.start_date)} - #{format_date(book.end_date)}"
     end
-
-    # Obtener el nombre del estado del libro o "Sin estado" si no tiene
-    def book_status_options(book)
-        book.status&.name || "Sin estado"
-    end
-
+    
+    # Generar una etiqueta HTML para el estado del libro con estilos específicos
     def book_status_label(book)
         case book.status&.name
         when "Pendiente"
