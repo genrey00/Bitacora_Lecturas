9ce1b526749c6e334c0128df95ec3282c9d2c122
Genesis <genesisreyes.9400@gmail.com>
2026-01-14 12:20:14 -0600

    update: agregar validaciones de nombre de autor y de ingreso de fecha

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,23 @@ class Book < ApplicationRecord
     validates :name_author, presence: true
     validates :last_name_author, presence: true
     validates :start_date, presence: true
-    
+    validate :end_date_after_start_date
+
+      # Concatenar el nombre y apellido del autor
+    def author_full_name
+        "#{name_author} #{last_name_author}"
+    end
+
+    private
+    def end_date_after_start_date
+        # Verifica que la fecha de finalización sea posterior a la fecha de inicio
+        return if end_date.blank? || start_date.blank?
+        # Agrega un error si la fecha de finalización es anterior a la fecha de inicio
+        if end_date < start_date
+            errors.add(:base, "La fecha de finalización debe ser posterior a la fecha de inicio")
+        end
+    end
+
     # relaciones con otros modelos
     belongs_to :status, optional: true
 end
